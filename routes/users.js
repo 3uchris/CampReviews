4d39a1ceee35c56e38906210a91798f287ce67e4
Chris Wu <yiming19@ualberta.ca>
2022-08-31 21:58:16 -0600

    added authorization for reviews and fixed errors of logout

--- routes/users.js
+++ routes/users.js
@@ -35,11 +35,12 @@ router.post('/login', passport.authenticate('local', { failureFlash: true, failu
     res.redirect(redirectUrl);
 })
 
-router.get('/logout', (req, res) => {
-    req.logout(err);
-    
-    req.flash('success', "Goodbye!");
+router.get('/logout', (req, res, next) => {
+    req.logout(function(err){
+        return next(err);
+    });
+    req.flash('success', 'Goodbye!');
     res.redirect('/campgrounds');
-})
+});
 
 module.exports = router;
--- seeds/index.js
+++ seeds/index.js
@@ -24,6 +24,7 @@ const seedDB = async () => {
         const random1000 = Math.floor(Math.random() * 1000);
         const price = Math.floor(Math.random() * 20) + 10
         const camp = new Campground({
+            author: '630f11a1ee7c71f091531e43',
             location: `${cities[random1000].city}, ${cities[random1000].state}`,
             title: `${sample(descriptors)} ${sample(places)}`,
             image: 'https://source.unsplash.com/collection/1368979',
